8478329c50884a7e2ace6a2eb226dce068d55554
Thomas Hynard <tomhynard@gmail.com>
2017-11-28 10:01:53 +1100

    Adding build task for deployment with Netlify

---
+++ webpack.config.build.js
@@ -0,0 +1,64 @@
+const { readFileSync } = require('fs');
+const webpack = require('webpack');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
+const treeShakenMinifyer = new webpack.LoaderOptionsPlugin({
+  minimize: true,
+  debug: false
+});
+
+const extractSharedLibraries = new webpack.optimize.CommonsChunkPlugin({
+  name: 'vendor',
+  filename: 'vendor.js'
+});
+
+const babelSettings = JSON.parse(readFileSync('.babelrc'));
+
+module.exports = {
+  entry: {
+    'index': [ './src/index.js' ]
+  },
+  plugins: [
+    extractSharedLibraries,
+    treeShakenMinifyer,
+    new webpack.NamedModulesPlugin(),
+    new UglifyJSPlugin({
+      uglifyOptions: {
+        beautify: false,
+        ecma: 6,
+        compress: true,
+        comments: false
+      }
+    })
+  ],
+  resolve: {
+    extensions: [ '.js', '.html' ]
+  },
+  output: {
+    path: __dirname + '/public',
+    filename: '[name].js',
+    chunkFilename: '[name].[id].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(html|js)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          query: babelSettings
+        }
+      },
+      {
+        test: /\.html$/,
+        exclude: /node_modules/,
+        use: 'svelte-loader'
+      },
+      {
+        test: /\.css$/,
+        use: [ 'style-loader', 'css-loader' ]
+      }
+    ]
+  },
+  devtool: 'cheap-module-eval-source-map'
+};
